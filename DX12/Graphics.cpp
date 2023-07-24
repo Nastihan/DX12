02095e80ed6da87e5e77a55c09c5f878db992308
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-25 01:42:31 +0330

    choose the descrete gpu when creating DX12 device

--- DX12/Graphics.cpp
+++ DX12/Graphics.cpp
@@ -11,9 +11,11 @@ Graphics::Graphics(uint16_t width, uint16_t height)
 	constexpr UINT bufferCount = 2;
 
 	// dxgi factory
-	Microsoft::WRL::ComPtr<IDXGIFactory4> dxgifactory;
-	CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&dxgifactory)) >> chk;
+	Microsoft::WRL::ComPtr<IDXGIFactory4> pdxgifactory;
+	CreateDXGIFactory2(DXGI_CREATE_FACTORY_DEBUG, IID_PPV_ARGS(&pdxgifactory)) >> chk;
+	Microsoft::WRL::ComPtr<IDXGIAdapter> pAdapter;
+	pdxgifactory->EnumAdapters(1U, pAdapter.GetAddressOf());
 
 	// device
-	D3D12CreateDevice(nullptr, D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pDevice));
+	D3D12CreateDevice(pAdapter.Get(), D3D_FEATURE_LEVEL_12_1, IID_PPV_ARGS(&pDevice));
 }
