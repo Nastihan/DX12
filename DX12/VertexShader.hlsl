19509a78b8c4803dafb9f05b9bc5595beeb00c9a
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-07-30 15:51:43 +0330

    use hlsl 5 syntax for cbuffers

--- DX12/VertexShader.hlsl
+++ DX12/VertexShader.hlsl
@@ -11,15 +11,16 @@ struct VS_Output
     float4 Color : COLOR;
 };
 
-cbuffer rotation
+struct rotation
 {
     matrix rot;
 };
+ConstantBuffer<rotation> rot : register(b0);
 
 VS_Output main( VS_Input input )
 {
     VS_Output output;
-    output.Position = mul(float4(input.pos, 1.0f), rot);
+    output.Position = mul(float4(input.pos, 1.0f), rot.rot);
     output.Color = float4(input.Color, 1.0f);
     return output;
 }
