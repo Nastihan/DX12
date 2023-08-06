05ee2f26ad881ac709a2f15bc5eb14055153bb40
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-06 17:15:50 +0330

    basic pointlight class

---
+++ DX12/PointLight.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "Graphics.h"
+#include "Sphere.h"
+
+class PointLight
+{
+public:
+	PointLight(Graphics& gfx);
+	void Draw(Graphics& gfx);
+
+private:
+	struct PointLightCBuf
+	{
+		alignas(16) DirectX::XMFLOAT3 pos = { 0.0f,0.0f,0.0f };
+		alignas(16) DirectX::XMFLOAT3 ambient = { 0.01f, 0.01f, 0.01f };
+		alignas(16) DirectX::XMFLOAT3 diffuseColor = { 1.0f, 1.0f, 1.0f };
+		float diffuseIntensity = 1.0f;
+		float attConst = 1.0f;
+		float attLin = 0.045f;
+		float attQuad = 0.0075f;
+	};
+private:
+	PointLightCBuf cBufData = {};
+	mutable Sphere mesh;
+};
