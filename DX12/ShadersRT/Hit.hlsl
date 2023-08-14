d2e243a31e4a57483f90ba25bf67285d116fb772
Nastihan Beheshtypour <nastihanb@gmail.com>
2023-08-14 22:23:09 +0330

    Seems like the HLSL dxr intrinsic functions are not declared!

--- DX12/ShadersRT/Hit.hlsl
+++ DX12/ShadersRT/Hit.hlsl
@@ -3,5 +3,8 @@
 [shader("closesthit")] 
 void ClosestHit(inout HitInfo payload, Attributes attrib) 
 {
-  payload.colorAndDistance = float4(1, 1, 0, RayTCurrent());
+    uint2 launchIndex = DispatchRaysIndex().xy;
+    float2 dims = float2(DispatchRaysDimensions().xy);
+    float ramp = launchIndex.y / dims.y;
+    payload.colorAndDistance = float4(0.0f, 0.2f, 0.7f - 0.3f * ramp, -1.0f);
 }
